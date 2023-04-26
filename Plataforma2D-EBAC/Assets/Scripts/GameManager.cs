58ecc50a37d7b5901a9499f816cdfdaae95e0a69
Denis Espanhol <denisgfespanhol@gmail.com>
2023-04-26 11:29:25 -0300

    Add principal Headers (Player and Enemies) in GameManager Script

--- Plataforma2D-EBAC/Assets/Scripts/GameManager.cs
+++ Plataforma2D-EBAC/Assets/Scripts/GameManager.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    [Header("Player")]
+    [SerializeField] private GameObject player;
+
+    [Header("Enemies")]
+    [SerializeField] private List<GameObject> enemies;
 }
