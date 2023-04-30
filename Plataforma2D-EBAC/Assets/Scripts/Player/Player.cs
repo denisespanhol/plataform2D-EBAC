eb737d14dd8a73e7b359c0635d82c9c6645dcf99
Denis Espanhol <denisgfespanhol@gmail.com>
2023-04-30 18:37:37 -0300

    Add Jump and creating function to move and jump

--- Plataforma2D-EBAC/Assets/Scripts/Player/Player.cs
+++ Plataforma2D-EBAC/Assets/Scripts/Player/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Vector2 friction;
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
 
     private Rigidbody2D _Rb2D;
 
@@ -16,6 +17,12 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             // _Rb2D.MovePosition(_Rb2D.position + velocity * Time.deltaTime);
@@ -25,6 +32,23 @@ public class Player : MonoBehaviour
         {
             // _Rb2D.MovePosition(_Rb2D.position - velocity * Time.deltaTime);
             _Rb2D.velocity = new Vector2(-speed, _Rb2D.velocity.y);
+        }
+
+        if (_Rb2D.velocity.x > 0)
+        {
+            _Rb2D.velocity += friction;
+        }
+        else if (_Rb2D.velocity.x < 0)
+        {
+            _Rb2D.velocity -= friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _Rb2D.velocity = Vector2.up * jumpForce;
         }
     }
 }
