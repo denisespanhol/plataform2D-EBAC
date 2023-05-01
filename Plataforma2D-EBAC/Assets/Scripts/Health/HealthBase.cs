d8cad7b7a356f3484c54dfe2154e3ccebc018d59
Denis Espanhol <denisgfespanhol@gmail.com>
2023-05-01 13:30:20 -0300

    Creating Script to Player's Health

---
+++ Plataforma2D-EBAC/Assets/Scripts/Health/HealthBase.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    [SerializeField] private int startLife;
+    [SerializeField] private bool destroyOnKill;
+
+    private int _currentLife;
+    private bool _isDead = false;
+
+    private void Awake()
+    {
+        init();
+    }
+
+    private void init()
+    {
+        _isDead = false;
+        _currentLife = startLife;
+    }
+
+    public void Damage(int damage)
+    {
+        if (_isDead) return;
+
+        _currentLife -= damage;
+
+        if (_currentLife <= 0)
+        {
+            Kill();
+        }
+    }
+
+    private void Kill()
+    {
+        _isDead = true;
+
+        if (destroyOnKill)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
